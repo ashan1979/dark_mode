c0f706fad80ba57823b0bf6b1598b53f52e7be46
ashan <indrajee79@gmail.com>
2023-01-20 17:47:18 +0530

    js

--- data.js
+++ data.js
@@ -3,5 +3,7 @@ const articles = [
         id: 1,
         title: 'the WET Codbase',
         date: new Date(2022, 9, 4),
-    }
+        length: 11,
+        snippet: `I'm baby ramps kombucha gluten free swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
+    },
 ]
